264cfcbd092d64d613f4cd971701186a04ce6eb0
BonewheelMaster <bonewheelmaster@proton.me>
2025-05-08 16:49:02 -0400

    Update

--- board.py
+++ board.py
@@ -1,42 +1,126 @@
-# TODO: consider whether tiles should know their pos and display the right number,
-# or if higher levels should manage that.
-class tile:
-    def __init__(self, is_bomb: bool, display_char: str):
-        self.is_bomb = is_bomb
-        self.display_char = display_char
+import random
 
+class Board:
+	"""
+	The entire worldspace.
 
-class board:
-    def __init__(self, resolution_x: int, resolution_y: int, tile_display_states: dict = None) -> None:
-        if tile_display_states is None:
-            self.tile_display_states = {
-               #"checked: " + str(i): str(i) for i in range(10)
-               "unknown": "?"
-            }
+	The only data that are stored are the tiles that have been flagged
+	and the tiles that have been revealed. All other data (whether a
+	tile is a bomb, how many bombs are around a tile, etc.) are reproducibly
+	calculated via the seed.
+	"""
+	def __init__(self, seed: int, bomb_chance: float) -> None:
+		"""
+		Args:
+			seed (int): Initial seed to use. This of course determines
+				where all bombs are.
 
-        self.size_x = resolution_x
-        self.size_y = resolution_y
+			bomb_chance (float): The chance (from 0-1) for each tile to
+				be a bomb.
+		"""
+		self.seed = seed
+		self.bomb_chance = bomb_chance
 
-        self.board = [
-            [ tile(False, self.tile_display_states["unknown"]) ] * self.size_x
-        ] * self.size_y
+		self.checked_tiles = []
+		self.flagged_tiles = []
 
-    # TODO: fix or remove me
-    def set_tile(self, pos_x: int, pos_y: int, state: str) -> None:
-        # Argument Validation
-        if any([
-                (0 > pos_x or pos_x > len(board[0])),
-                (0 > pos_y or pos_y > len(board)),
-                state not in self.tile_display_states.keys()
-            ]):
-            raise ValueError(
-                "pos_x must be an integer between 0 and {}, ".format(len(board[0]))
-                + "pos_y must be an integer between 0 and {}, and ".format(len(board))
-                + "state must be a str whose value is in {}.".format(list(tile_states.keys()))
-            )
+	def check_tile(self, position_x: int, position_y: int) -> None:
+		"""
+		Set tile as checked, ending the game if it is a bomb.
 
-        pass
+		Args:
+			position_x (int): Position of the tile on the x axis. Higher values
+				go right.
+				
+			position_y (int): Position of the title on the y axis. Higher values
+				go down.
+		"""
+		self.checked_tiles.append((position_x, position_y))
 
-    def get_tile(self, pos_x: int, pos_y: int) -> tile:
-        return self.board[pos_y][pos_x]
+		if self.is_bomb(position_x, position_y):
+			self.end_game()
 
+	def get_tile_state(self, position_x: int, position_y: int) -> string:
+		"""
+		Get the state of a tile for the purpose of displaying on screen.
+
+		Returns: 
+			string: One of the following:
+			- "checked: i", where i is an int [0-9]
+			- "flagged"
+			- "hidden"
+		"""
+		if (position_x, position_y) in self.checked_tiles:
+			return "checked: {}".format(
+				get_adjacent_tile_counts(position_x, position_y)["bombs"]
+			)
+
+		elif (position_x, position_y) in self.flagged_tiles:
+			return "flagged"
+
+		else:
+			return "hidden"
+
+	def is_bomb(self, position_x: int, position_y: int):
+		"""
+		Check if given tile is a bomb.
+
+		Generates a random number from 0-1 using the seed mixed with the tile's
+		positions. This is then compared with the bomb chance, and, if the chance
+		is higher, the tile is a bomb.
+
+		Args:
+			position_x (int): Position of the tile on the x axis. Higher values
+				go right.
+				
+			position_y (int): Position of the title on the y axis. Higher values
+				go down.
+
+		Return
+			bool: True if given tile is a bomb, False otherwise.
+		"""
+		random.seed(self.seed + position_x + position_y)
+
+		roll = random.randint(1, 100)
+		return self.bomb_chance >= roll
+
+	def get_adjacent_tile_counts(self, position_x: int, position_y: int):
+		"""
+		Get the counts of the eight adjacent tiles to the tile specified.
+
+		Counts retrieved are: number of flags and number of bombs in adjacent tiles.
+
+		Args:
+			position_x (int): Position of the tile on the x axis. Higher values
+				go right.
+
+			position_y (int): Position of the tile on the y axis. Higher values
+				go down.
+
+		Returns:
+			dict: The keys (strings) are "bombs" and "flags" and the values (ints)
+				are the counts of each.
+		"""
+		count_bombs = 0
+		count_flags = 0
+
+		for i in range(-1, 2): # Range end is exclusive.
+			for v in range(-1, 2):
+				# TODO: verify this works correctly.
+				if i == 0 and v == 0:
+					continue
+
+				if is_bomb(position_x + i, position_y + v):	
+					count_bombs += 1
+
+				if (position_x + i, position_y + v) in self.flagged_tiles:
+					count_flags += 1
+
+		return {"bombs": count_bombs, "flags": count_flags}
+
+	def end_game(self):
+		"""
+		End the game as a result of revealing a bomb.
+		"""
+		pass
+					
--- infsweep.py
+++ infsweep.py
@@ -1,14 +1,16 @@
 import board
+import screen
 
-game_board = board.board(21, 6)
+game_board = board.Board(12345, 5)
+game_screen = screen.Screen(21, 6)
 
-for i in range(game_board.size_y):
-    for v in range(game_board.size_x):
-        print(game_board.get_tile(v, i).display_char, end="")
+#for i in range(game_board.size_y):
+#	for v in range(game_board.size_x):
+#		print(game_board.get_tile(v, i).display_char, end="")
 
-    print()
-    
+#	print()
+
 input()
 
-game_board.set_tile(5, 5, "checked: 3")
-game_board.check(2, 6)
+#game_board.set_tile(5, 5, "checked: 3")
+game_board.check_tile(2, 6)
