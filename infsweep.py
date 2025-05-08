925c34253d2f35dcaf5f3d584a3601c878a70452
BonewheelMaster <bonewheelmaster@proton.me>
2025-05-07 23:16:25 -0400

    Setup as main

--- infsweep.py
+++ infsweep.py
@@ -1,14 +1,14 @@
-import casioplot as p
+import game_board
 
+board = game_board.board(21, 6)
 
-def main() -> None:
-	p.show_screen()
-	# Note: A character is 3x5px (HxV). This becomes 5x7px with a 1 px border. 
-	# However, the border is not present on the edges, as it is unnecessary. 
-	# This means that the screen resolution (128x64px) can be considered 130x66px.
-	for i in range(11):
-		for v in range(26):
-			p.draw_string(v*5, i*6, "2", )
+for i in range(board.size_y):
+    for v in range(board.size_x):
+        print(board.get_tile(v, i).display_char, end="")
 
+    print()
+    
+input()
 
-main()
+board.set_tile(5, 5, "checked: 3")
+board.check(2, 6)
