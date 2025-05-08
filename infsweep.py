d557c512acdacceee5d862063cda421bdba57906
BonewheelMaster <bonewheelmaster@proton.me>
2025-05-07 23:21:27 -0400

    Rename to fit within 8 chars
    Updated infsweep.py to not override the module with the variable it binds the class to, as the module's name has now changed.

--- infsweep.py
+++ infsweep.py
@@ -1,14 +1,14 @@
-import game_board
+import board
 
-board = game_board.board(21, 6)
+game_board = board.board(21, 6)
 
-for i in range(board.size_y):
-    for v in range(board.size_x):
-        print(board.get_tile(v, i).display_char, end="")
+for i in range(game_board.size_y):
+    for v in range(game_board.size_x):
+        print(game_board.get_tile(v, i).display_char, end="")
 
     print()
     
 input()
 
-board.set_tile(5, 5, "checked: 3")
-board.check(2, 6)
+game_board.set_tile(5, 5, "checked: 3")
+game_board.check(2, 6)
