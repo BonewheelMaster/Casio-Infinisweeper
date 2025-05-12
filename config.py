60d436318e1849e12ab681fa788f74b1cf8cb22f
BonewheelMaster <bonewheelmaster@proton.me>
2025-05-12 16:21:00 -0400

    Initial commit

---
+++ config.py
@@ -0,0 +1,22 @@
+# [SCREEN]
+# Number of characters to be displayed per line.
+RESOLUTION_X = 21
+# Number of lines to be displayed.
+RESOLUTION_Y = 6
+# Which characters should be mapped to which states.
+DISPLAY_MAP = {
+	"flagged": "F",
+	"hidden": "?",
+	"bomb": "@",
+	"checked: 0": " ",
+}
+DISPLAY_MAP.update({"checked: {}".format(i): str(i) for i in range(1, 10)})
+# Character to display for the cursor. This replaces whatever character the
+# cursor is over.
+CURSOR_CHAR = "#"
+
+# [BOARD]
+# Seed to use for the map. Changing the seed changes where bombs are placed.
+SEED = 12345
+# Chance [0-100] for any given tile to be a bomb.
+BOMB_CHANCE = 5
---
+++ utils.py
@@ -0,0 +1,2 @@
+def clamp(n, minimum_value, maximum_value):
+	return max( minimum_value, min(n, maximum_value) )
