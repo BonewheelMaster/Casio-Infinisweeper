8c14f81c3bb681d7ca53e3187de280ca0fbc5c45
BonewheelMaster <bonewheelmaster@proton.me>
2025-05-12 16:20:01 -0400

    Remove unnecessary lines

--- infsweep.py
+++ infsweep.py
@@ -1,18 +1,6 @@
-import board
 import screen
 import usrinput
 
-game_board = board.Board(12345, 5)
-game_screen = screen.Screen(21, 6)
-
-#for i in range(game_board.size_y):
-#	for v in range(game_board.size_x):
-#		print(game_board.get_tile(v, i).display_char, end="")
-
-#	print()
-
 while True:
+	screen.update()
 	usrinput.take_input()
-
-#game_board.set_tile(5, 5, "checked: 3")
-game_board.check_tile(2, 6)
