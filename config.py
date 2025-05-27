8938fd4baaff5c1d62a155d6b8e46a434d486ab9
BonewheelMaster <bonewheelmaster@proton.me>
2025-05-27 19:30:08 -0400

    Move key map to here
    Also, change "checked: 0" to "safe".

--- config.py
+++ config.py
@@ -1,5 +1,4 @@
-"""
-"""
+"""Configuration options"""
 # [SCREEN]
 # Number of characters to be displayed per line.
 RESOLUTION_X = 21
@@ -11,17 +10,43 @@ DISPLAY_MAP = {
 	"flagged": "F",
 	"hidden": "?",
 	"bomb": "@",
-	"checked: 0": " ",
+	"safe": " ",
 }
 DISPLAY_MAP.update({"checked: {}".format(i): str(i) for i in range(1, 10)})
 # Character to display for the cursor. This replaces whatever character the
 # cursor is over.
 CURSOR_CHAR = "#"
 
+
 # [BOARD]
 # Seed to use for the map. Changing the seed changes where bombs are placed.
 SEED = 12345
 # Chance [0-100] for any given tile to be a bomb.
 BOMB_CHANCE = 5
-# Number of tiles to cache. 0 turns caching off. One tile costs ~50B.
-CACHE_SIZE = (RESOLUTION_X * RESOLUTION_Y) + 25
+# Whether to automatically uncover all tiles around a tile with no bombs
+# around it.
+AUTO_UNCOVER_TILES = True
+
+
+# [INPUT]
+# The mapping of key commands to functions. These functions are defined
+# in usrinput.py.
+INPUT_MAP = {
+	"4": lambda board: move(-1, 0),
+	"8": lambda board: move(0, -1),
+	"6": lambda board: move(1, 0),
+	"2": lambda board: move(0, 1),
+	"1": lambda board: move(-1, 1),
+	"7": lambda board: move(-1, -1),
+	"9": lambda board: move(1, -1),
+	"3": lambda board: move(1, 1),
+	"5": lambda board: board.flag_tile(
+		screen.screen_position_x + screen.cursor_position_x,
+		screen.screen_position_y + screen.cursor_position_y
+	),
+	"0": lambda board: board.check_tile(
+		screen.screen_position_x + screen.cursor_position_x,
+		screen.screen_position_y + screen.cursor_position_y
+	),
+	".": lambda board: toggle_movement_mode(),
+}
