8cf45050fb1f84a2494d2c495bc807cd31b3a160
BonewheelMaster <bonewheelmaster@proton.me>
2025-05-27 19:31:48 -0400

    Move key map to config
    Also update documentation and remove debugging function.

--- usrinput.py
+++ usrinput.py
@@ -2,50 +2,34 @@
 Receive input and interpret it.
 
 Attributes:
+	Defined in config:
+		INPUT_MAP: The map of input characters to functions.
 """
 import screen
-import board
 import utils
 import config
 
-# Move to config
-input_map = {
-	"4": lambda: move(-1, 0),
-	"8": lambda: move(0, -1),
-	"6": lambda: move(1, 0),
-	"2": lambda: move(0, 1),
-	"1": lambda: move(-1, 1),
-	"7": lambda: move(-1, -1),
-	"9": lambda: move(1, -1),
-	"3": lambda: move(1, 1),
-	"5": lambda: board.flag_tile(
-		screen.screen_position_x + screen.cursor_position_x,
-		screen.screen_position_y + screen.cursor_position_y
-	),
-	"0": lambda: board.check_tile(
-		screen.screen_position_x + screen.cursor_position_x,
-		screen.screen_position_y + screen.cursor_position_y
-	),
-	".": lambda: toggle_movement_mode(),
-	"/": lambda: debug_reveal_all()
-}
+INPUT_MAP = config.INPUT_MAP
 
 # Move the screen instead of the cursor if True. Modified
 # by the toggle_movement_mode function.
-screen_move = False
+_screen_move = False
 
 
-def take_input() -> None:
+def take_input(board) -> None:
 	"""
 	Wait for input and run the according function/s.
 
-	If the user inputs multiple commands, all will be run.
+	If the user inputs multiple commands, all will be run in order.
+	
+	Args:
+		board: The board object to modify. Must have flag_tile and check_tile functions.
 	"""
 	for char in input(generate_prompt()):
-		if char not in input_map.keys():
+		if char not in INPUT_MAP:
 			continue
 
-		input_map[char]()
+		INPUT_MAP[char](board)
 
 
 def generate_prompt() -> str:
@@ -56,9 +40,9 @@ def generate_prompt() -> str:
 		- Mode of movement (cursor or screen)
 
 	Returns:
-		string: The prompt.
+		The prompt.
 	"""
-	prompt = "CUR" if not screen_move else "SCR"
+	prompt = "CUR" if not _screen_move else "SCR"
 	prompt += "> "
 
 	return prompt
@@ -68,14 +52,16 @@ def move(offset_x: int, offset_y: int) -> None:
 	"""
 	Move the cursor or the screen.
 
+	Decides between cursor or screen using internal state.
+
 	Args:
-		offset_x (int): Amount to move the cursor or screen on the x-axis. Positive
+		offset_x: Amount to move the cursor or screen on the x-axis. Positive
 			values move them right, while negative values move them left.
 
-		offset_y (int): Amount to move the cursor or screen on the y-axis. Positive
+		offset_y: Amount to move the cursor or screen on the y-axis. Positive
 			values move them down, while negative values move them up.
 	"""
-	if screen_move:
+	if _screen_move:
 		screen.screen_position_x += offset_x
 		screen.screen_position_y += offset_y
 	
@@ -83,23 +69,21 @@ def move(offset_x: int, offset_y: int) -> None:
 		screen.cursor_position_x += offset_x
 		screen.cursor_position_y += offset_y
 
+		# Prevent cursor from going off screen.
 		screen.cursor_position_x = utils.clamp(
 			screen.cursor_position_x,
 			0,
-			screen.RESOLUTION_X-1
+			screen.RESOLUTION_X - 1
 		)
 		screen.cursor_position_y = utils.clamp(
 			screen.cursor_position_y,
 			0,
-			screen.RESOLUTION_Y-1
+			screen.RESOLUTION_Y - 1
 		)
 
 
 def toggle_movement_mode() -> None:
-	global screen_move
+	"""Switch between moving the screen and moving the cursor."""
+	global _screen_move
 	
-	screen_move = not screen_move
-
-
-def debug_reveal_all() -> None:
-	board.debug_revealed = not board.debug_revealed
+	_screen_move = not _screen_move
