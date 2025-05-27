db20839a9b685fa6564814b912b7135ef0b6420e
BonewheelMaster <bonewheelmaster@proton.me>
2025-05-27 19:27:47 -0400

    Interface with the Board class
    Board is a class now. This makes it possible to have multiple boards, though I don't intend to add this functionality.

--- infsweep.py
+++ infsweep.py
@@ -1,6 +1,12 @@
 import screen
 import usrinput
+import board
+import config
+
+game_board = board.Board(
+	config.SEED, config.BOMB_CHANCE, config.AUTO_UNCOVER_TILES
+)
 
 while True:
-	screen.update()
-	usrinput.take_input()
+	screen.update(game_board)
+	usrinput.take_input(game_board)
