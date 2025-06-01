2dc6018cad4768bdf2b433fa09ef0ac30ff6f3ef
BonewheelMaster <bonewheelmaster@proton.me>
2025-05-31 21:16:59 -0400

    Remove hardcoded default character
    I forgot to add it. Oops.

--- board.py
+++ board.py
@@ -73,7 +73,10 @@ class Board:
                 on those rows are the columns (x). This means that to access
                 the tile (x, y), use something like `tile_grid[y][x]`.
         """
-        tile_grid = [["?" for _ in range(resolution_x)] for _ in range(resolution_y)]
+        tile_grid = [
+            [DISPLAY_MAP["hidden"] for _ in range(resolution_x)]
+                for _ in range(resolution_y)
+        ]
 
         bombs = self._get_bombs_on_screen(
             screen_position_x, screen_position_y, resolution_x, resolution_y
